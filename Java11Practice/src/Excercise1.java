3ef72dd09200c36f2fd9424d81c9dfe33bd4bcda
EduardoBtz <eduardombc96@gmail.com>
2021-06-25 18:01:44 -0500

    Added Excercices 1 and 2.

---
+++ Java11Practice/src/Excercise1.java
@@ -0,0 +1,23 @@
+public class Excercise1 {
+    public static void main(String[] args) {
+        String sister = "Sarita Benitez";
+        String father = "Eduardo Benitez Proa";
+        String mother = "Maria Antonia Carrillo Obregón";
+        String me = "Eduardo Missael Benitez Carrillo";
+
+        int sisterAge = 22;
+        int dadAge = 52;
+        int momAge = 52;
+        int myAge = 25;
+
+        System.out.println("Hi Everyone! My name is " + me + ", I am " + myAge + " years old " +
+                "and this is my Family!");
+        System.out.println("My dad's name is " + father + " and he is " + dadAge + " years old.");
+        System.out.println("My moms's name is " + mother + " and she is " + momAge + " years old.");
+        System.out.println("My sister's name is " + sister + " and she is " + sisterAge + " years old.");
+
+        String city = "Monterrey";
+        String country = "Mexico";
+        System.out.println("We are from + " + city + ", " + country + " and we love each other.");
+    }
+}
---
+++ Java11Practice/src/Excercise2.java
@@ -0,0 +1,33 @@
+public class Excercise2 {
+    public static void main(String[] args) {
+
+        // Cast char as int.
+        char c = (int) 'z';
+        System.out.println(c);
+
+        // Int to long, long to short.
+        int a = 250;
+        long b = a;
+        System.out.println((short) b);
+
+        // Double to long.
+        double d = 301.067;
+        System.out.println((long) d);
+
+        // Int to float.
+        int i = 100;
+        i += 500.66;
+        System.out.println((float) i);
+
+        // Int to byte
+        int x = 737;
+        x *= 100;
+        System.out.println((byte) x);
+
+        // Double to long.
+        double y = 298.638;
+        y /= 25;
+        System.out.println((long) y);
+
+    }
+}
