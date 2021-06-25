f4d6169a2b7b9d90eecb46b1e4253aef27ad9cee
EduardoBtz <eduardombc96@gmail.com>
2021-06-25 14:41:00 -0500

    Practiced with Naming conventions in Java and numeric data types.

---
+++ Java8Practice/src/NamingJava.java
@@ -0,0 +1,22 @@
+            // Upper Cammel Case
+public class NamingJava {
+            // Lower Camel Case for variables and methods.
+    public static void main(String[] args) {
+        int celphone = 3333333;
+        int celPhone = 3123123;
+        System.out.println(celphone);
+        System.out.println(celPhone);
+
+        String $countryName = "Mexico";
+        String _backgroundColor = "Blue";
+
+        String currency$ = "MXN";
+        String background_color = "BLUE";
+
+        int POSITION = -5;
+        int MAX_WIDTH = 9999;
+        final int MIN_WIDTH = 1;
+        final double PI = 3.141620;
+
+    }
+}
