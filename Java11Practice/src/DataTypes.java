9712e42dd0e76469bf79ef11a894842d02c6a6af
EduardoBtz <eduardombc96@gmail.com>
2021-06-25 17:32:21 -0500

    Practiced Casting, Increment, Naming and Arithmetic operations in Java.

--- Java11Practice/src/DataTypes.java
+++ Java11Practice/src/DataTypes.java
@@ -1,2 +1,29 @@
-package PACKAGE_NAME;public class DataTypes {
+public class DataTypes {
+    public static void main(String[] args) {
+        int n = 1234567890;
+        long nL = 12345678901L;
+
+        double nD = 123.456;
+        float nF = 123.456F;
+
+        // Java Infers data types.
+        var salary = 100; // Int
+        var morgage = salary*0.03; // double
+        var totalSalary = salary - morgage; // double
+
+        System.out.println(salary);
+        System.out.println(morgage);
+
+        var employeeName = "Eduardo Benitez"; // String
+        System.out.println("Employee: " + employeeName + "Salary: " + totalSalary);
+
+
+
+
+    }
 }
+
+
+
+
+
